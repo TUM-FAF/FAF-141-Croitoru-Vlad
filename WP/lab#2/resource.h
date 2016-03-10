5d5bf78a6b1cabec2bc3f9cac8c607d3fa7fca27
VladCroitoru <0croitoruvlad@gmail.com>
2016-03-11 00:29:14 +0200

    trump

---
+++ WP/lab#2/resource.h
@@ -0,0 +1,29 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+
+#define IDC_LIST_BOX            100
+#define IDC_NEW_ITEM            101
+
+#define IDC_ADD_BUTTON          102
+#define IDC_REMOVE_BUTTON       103
+#define IDC_CLEAR_BUTTON        104
+
+
+#define IDC_FILE_EXIT           105
+#define IDC_HELP_ABOUT          106
+
+#define IDC_HEIGHT_SCROLL       107
+#define IDC_WIDTH_SCROLL        108
+#define IDC_MOVE                109
+
+#define IDI_ICON                110
+#define IDC_CURSOR              111
+
+#define textColor               RGB(84,121,128)
+#define bkColor                 RGB(157,224,173)
+
+#define IDB_BACKGROUND              700
+
+#define IDD_ABOUT 1000
